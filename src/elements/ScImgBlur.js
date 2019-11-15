3e1ac75da9b3517eba8539422875a6eb83eb8218
sdiperi17 <sdiperi17@gmail.com>
2019-11-14 21:34:22 -0500

    fixed image hover

--- src/elements/ScImgBlur.js
+++ src/elements/ScImgBlur.js
@@ -3,36 +3,49 @@ import styled from "styled-components";
 
 export const ScImg = styled.img`
     height: 100%;
-    // to center the image inside the shape
-    transform: translateX(-1rem) scale(1.4);
+    transform: translateX(-0.3rem) scale(1.3);
     backface-visibility: hidden;
     transition: all 0.5s;
+    
 
     &:hover {
         transform: translateX(-4rem) scale(1);
-
-        // to make the img blured and darker on hover. Value lower than 100% makes
-        // it darker and above 100% makes it lighter
         filter: blur(3px) brightness(80%);
+        border-radius: 50%
     }
 `;
 
 export const ScImgContainer = styled.div`
     display: flex;
-
     justify-content: center;
     height: 150px;
     width: 100%;
-
     box-shadow: 0 3rem 6rem rgba($color-black, 0.1);
-    /* background-color: rgba(250, 250, 250, 0.6); */
     border-radius: 100px;
     position: relative;
     z-index: 3;
+    &:hover{
+        border-radius: 100px;
+        
+    }
+`;
+export const ScFigCaption = styled.figcaption`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 20%);
+    color: white;
+    text-transform: uppercase;
+    font-size: 1.7rem;
+    text-align: center;
+    opacity: 0;
+    transition: all 0.5s;
+    backface-visibility: hidden;
+
 `;
 
 export const ScFigure = styled.figure`
-    width: 150px;
+
     height: 150px;
 
     position: absolute;
@@ -46,28 +59,18 @@ export const ScFigure = styled.figure`
         transform: translateX(-1rem) scale(1.2);
 
         filter: blur(3px) brightness(80%);
+        border-radius: 100px;
+
     }
-`;
-
-export const ScFigCaption = styled.figcaption`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, 20%);
-    color: white;
-    text-transform: uppercase;
-    font-size: 1.7rem;
-    text-align: center;
-    opacity: 0;
-    transition: all 0.5s;
-    backface-visibility: hidden;
-    // removes little shake or gli
-
-    &:hover {
+    &:hover ${ScFigCaption} {
         opacity: 1;
         transform: translate(-50%, -50%);
+        border-radius: 50%
+
     }
 `;
+
+
 
 export const ScImgBlur = props => {
     return (
